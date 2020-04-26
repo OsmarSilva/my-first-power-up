253230d80b36b7932b3faf7677ba45fbb4cb7f7f
Osmar Silva <osmar.silva@grupoicts.com.br>
2020-04-26 00:01:12 -0400

    updated card-button to show estimate size

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,3 +1,7 @@
+const GREY_ROCKET_ICON =
+  'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
+const WHITE_ROCKET_ICON =
+  'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
 console.log('Hello World');
 
 window.TrelloPowerUp.initialize({
@@ -17,12 +21,14 @@ window.TrelloPowerUp.initialize({
     ];
   },
   'card-badges': function (t, options) {
-    return [
-      {
-        icon:
-          'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
-        text: '3',
-      },
-    ];
+    return t.get('card', 'shared', 'estimate').then(function (estimate) {
+      return [
+        {
+          icon: estimate ? GREY_ROCKET_ICON : WHITE_ROCKET_ICON,
+          text: estimate || 'No Estimate!',
+          color: estimate ? null : 'red',
+        },
+      ];
+    });
   },
 });
--- src/js/estimate.js
+++ src/js/estimate.js
@@ -9,5 +9,12 @@ window.estimate.addEventListener('submit', function (event) {
 });
 
 t.render(function () {
-  t.sizeTo('#estimate').done();
+  return t
+    .get('card', 'shared', 'estimate')
+    .then(function (estimate) {
+      window.estimateSize.value = estimate;
+    })
+    .then(function () {
+      t.sizeTo('#estimate').done();
+    });
 });
